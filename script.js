a8395391f08d4178fc8dc965ea8331b672a8c8db
samadova.minira <minira.samadova@syncthought.com>
2019-03-20 15:45:28 +0900

    finally finished with adding events

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-// $(".add-task").click( function() {
-//   $("#list").prepend("<li class=\"task-item\"><input type=\"checkbox\" class=\"done\"></button>" + $("#input").val() + "</li>");
-//   $("input.done").click( function() {
-//     console.log('Adding class');
-//     var $todo = $(this).closest('li');
-//     $todo.addClass("delete-text");
-//     setTimeout(function (){
-//       console.log('Removing a todo');
-//       $todo.addClass(".done").remove();
-//     },800);
-//   });
-// });
+//Press enter to add new task
+
+$("#input").keypress( function() {
+  //trigger if the key is enter
+  if( event.type == "keypress" && event.which === 13 ) {
+    //add new task
+    var $li = $("<li class=\"task-item\"><input type=\"checkbox\" class=\"done\"></button>" + $("#input").val() + "</li>");
+    //put it in the beginning of list
+    $("#list").prepend($li);
+    //clear imputted value
+    $("#input").val("");
+    $li.find("input.done").click( function() {
+      $li.addClass("cross-out-text");
+      setTimeout(function (){
+        $li.addClass(".done").remove();
+      },800);
+    });
+  };
+});
+
+//Press button to add new task
 
 $(".add-task").click( function() {
-  var $li = $("<li class=\"task-item\"><input type=\"checkbox\" class=\"done\"></button>" + $("#input").val() + "</li>");
-  $("#list").prepend($li);
-  //clear imputted value
-  $("#input").val(" ");
-  $li.find("input.done").click( function() {
-    $li.addClass("cross-out-text");
-    setTimeout(function (){
-      $li.addClass(".done").remove();
-    },800);
-  });
+    //add new task
+    var $li = $("<li class=\"task-item\"><input type=\"checkbox\" class=\"done\"></button>" + $("#input").val() + "</li>");
+    //put it in the beginning of list
+    $("#list").prepend($li);
+    //clear imputted value
+    $("#input").val("");
+    $li.find("input.done").click( function() {
+      $li.addClass("cross-out-text");
+      setTimeout(function (){
+        $li.addClass(".done").remove();
+      },800);
+    });
 });
