a192c6fa1f0c0b2e4f670e45b2a53407c0883d4d
samadova.minira <minira.samadova@syncthought.com>
2019-03-20 16:25:52 +0900

    added new function

--- script.js
+++ script.js
@@ -3,34 +3,53 @@
 $("#input").keypress( function() {
   //trigger if the key is enter
   if( event.type == "keypress" && event.which === 13 ) {
+
     //add new task
-    var $li = $("<li class=\"task-item\"><input type=\"checkbox\" class=\"done\"></button>" + $("#input").val() + "</li>");
+    var $li = $("<li class=\"task-item\"><input type=\"checkbox\" class=\"checkbox\"></button>" + "<p class=\"new-task\">" + $("#input").val() + "</p>" + "</li>");
+
     //put it in the beginning of list
     $("#list").prepend($li);
+
     //clear imputted value
     $("#input").val("");
-    $li.find("input.done").click( function() {
+
+    //delete task when clicked on
+    $li.find("p.new-task").click( function() {
       $li.addClass("cross-out-text");
       setTimeout(function (){
-        $li.addClass(".done").remove();
+        $li.addClass(".checkbox").remove();
       },800);
     });
   };
+
+  //Move tasks to Done section when checked
+  $li.find("input.checkbox").click( function() {
+    $li.prependTo("#done-tasks-list");
+  });
 });
 
 //Press button to add new task
 
 $(".add-task").click( function() {
     //add new task
-    var $li = $("<li class=\"task-item\"><input type=\"checkbox\" class=\"done\"></button>" + $("#input").val() + "</li>");
+    var $li = $("<li class=\"task-item\"><input type=\"checkbox\" class=\"checkbox\"></button>" + "<p class=\"new-task\">" + $("#input").val() + "</p>" + "</li>");
+
     //put it in the beginning of list
     $("#list").prepend($li);
+
     //clear imputted value
     $("#input").val("");
-    $li.find("input.done").click( function() {
+
+    //delete task when clicked on
+    $li.find("p.new-task").click( function() {
       $li.addClass("cross-out-text");
       setTimeout(function (){
-        $li.addClass(".done").remove();
+        $li.addClass(".checkbox").remove();
       },800);
+    });
+
+    //Move tasks to Done section when checked
+    $li.find("input.checkbox").click( function() {
+      $li.prependTo("#done-tasks-list");
     });
 });
