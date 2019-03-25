77e3288d960dcb69e13966421be6535a02c3fceb
samadova.minira <minira.samadova@syncthought.com>
2019-03-25 10:03:53 +0900

    solved click event overlap problem

--- script2.js
+++ script2.js
@@ -19,6 +19,7 @@ var lastTaskId = 0;
 
 function addTask(task) {
   allTasks.push({title: task, isDone: false, id: lastTaskId++});
+  $("#input").val("");
   render();
 }
 
@@ -27,8 +28,9 @@ function render() {
   for(var i = 0; i < allTasks.length; i++) {
     if(allTasks[i].isDone === true) {
       $("#done-tasks-list").prepend(`<li data-id="${allTasks[i].id}" class="task"><label>
-      <input type="checkbox" class="checkbox">
+      <input type="image" src="images/done-icon.png"> 
       <span class="new-task"> ${allTasks[i].title} </span></label></li>`);
+
     } else {
       $("#tasks").prepend(`<li data-id="${allTasks[i].id}" class="task"><label>
       <input type="checkbox" class="checkbox">
