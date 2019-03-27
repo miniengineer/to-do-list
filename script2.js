378fbd3cf368148ca3212c1c27ab667c65f99acf
samadova.minira <minira.samadova@syncthought.com>
2019-03-28 08:42:20 +0900

    added necessary functions

--- script2.js
+++ script2.js
@@ -3,9 +3,9 @@
 $("#input").keypress(function() {
   //trigger if the key is enter
   if( event.type == "keypress" && event.which === 13 ) {
-    // add new task
+    // if task don't exsists in to-do => add new task
     addTask($(this).val());
- };
+ }
 });
 
 //Press button to add new task
@@ -14,36 +14,70 @@ $(".add-task").click(function() {
   addTask($("#input").val());
 });
 
+//variables necessary for render function
+
 const allTasks = [];
 var lastTaskId = 0;
 
+//adding a task
+
 function addTask(task) {
-  allTasks.push({title: task, isDone: false, id: lastTaskId++});
-  render();
-  $("#input").val("");
+  //edge case for empty string
+  if(task === "") {
+    return alert("please enter a task");
+  }
+  //get rid of white space
+  task = task.trim();
+  //check if task already exists in to-do list
+  let currentTask = allTasks.find(function(e) {
+    return e.title === task && e.status === "toDo";
+  });
+  if(currentTask !== undefined) {
+    return alert("This task already exists");
+  } else {
+    allTasks.push({title: task, status: "toDo", id: lastTaskId++});
+    render();
+    $("#input").val("");
+  }
 }
 
+//render
+
 function render() {
+  //clear everything
   $("#tasks, #done-tasks-list").empty();
+  //decide whether it's done or a new task and prepend accordingly
   for(var i = 0; i < allTasks.length; i++) {
-    if(allTasks[i].isDone === true) {
+    //if task is checked move to done section
+    if(allTasks[i].status === "done") {
       $("#done-tasks-list").prepend(`<li data-id="${allTasks[i].id}" class="task"><label>
       <input type="image" src="images/done-icon.svg" class="icon">
       <span class="new-task"> ${allTasks[i].title} </span></label></li>`);
-
-    } else {
+    } else if(allTasks[i].status === "toDo") {
+      //if it's new move to to-do-list
       $("#tasks").prepend(`<li data-id="${allTasks[i].id}" class="task"><label>
       <input type="checkbox" class="checkbox">
       <span class="new-task"> ${allTasks[i].title} </span></label></li>`);
-    };
+    }
   };
-  // move to Done section
+  // move to done section when checked
   $("li.task .checkbox").click(function(e) {
     var id = $(this).closest(".task").data("id");
     var thisTask = allTasks.find(function(e) {
       return e.id == id;
     });
-    thisTask.isDone = true;
+    thisTask.status = "done";
+    render();
+  });
+  //remove task when clicked on
+  $(".new-task").click(function(e) {
+    var id = $(this).closest(".task").data("id");
+    var thisTask = allTasks.find(function(e) {
+      return e.id == id;
+    });
+    thisTask.status = "toDeletion";
+    //don't forget to remove it from array of tasks
+    allTasks.splice(allTasks.indexOf(thisTask),1);
     render();
   });
 }
